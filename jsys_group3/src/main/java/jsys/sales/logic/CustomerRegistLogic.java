af95d529aa432926d38c224f9e1434496e880556
kayoko0201 <sayoko.yui.929@gmail.com>
2024-08-08 10:19:56 +0900

    油井：action

---
+++ jsys_group3/src/main/java/jsys/sales/logic/CustomerRegistLogic.java
@@ -0,0 +1,16 @@
+package jsys.sales.logic;
+
+import jsys.sales.entity.Customer;
+
+public class CustomerRegistLogic {
+
+	public void CheckCustomer(Customer customer) {
+
+	}
+
+	public void insertCustomer(Customer customer) {
+
+	}
+
+
+}
