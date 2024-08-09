4f93b0ec78fda104118b8e25efa7d7d38cab02b3
NakaShun-lab <DLsite4010@gmail.com>
2024-08-09 09:34:26 +0900

    CustomerCodeDaoのテストドライバーを送ります。 中井より

---
+++ jsys_group3/src/main/java/jsys/sales/test/TestCustomerCodeDAO01_02.java
@@ -0,0 +1,50 @@
+/**
+ * @author J08_中井俊（2024/8/8）
+ */
+package jsys.sales.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import jsys.sales.dao.ConnectionManager;
+import jsys.sales.dao.CustomerCodeDAO;
+
+public class TestCustomerCodeDAO01_02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Connection con = null;
+
+		// テストのための準備としてデータベースに接続する。
+
+		try {
+			con = ConnectionManager.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// ここからテストを行う。
+		try {
+			CustomerCodeDAO custDAO = new CustomerCodeDAO(con);
+			int custList = custDAO.findCustomerCode();
+
+			if (custList == 0) {
+				System.out.println("戻り値：" + custList);
+			} else {
+				System.out.println("得意先コード：" + custList);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("SQLExceptionがスローされました。");
+			e.printStackTrace();
+		} finally {
+			try {// データベースへの接続を切断する
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ jsys_group3/src/main/java/jsys/sales/test/TestCustomerCodeDAO01_04.java
@@ -0,0 +1,45 @@
+/**
+ * @author J08_中井俊（2024/8/9）
+ */
+package jsys.sales.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;import jsys.sales.dao.ConnectionManager;
+import jsys.sales.dao.CustomerCodeDAO;
+
+public class TestCustomerCodeDAO01_04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Connection con = null;
+
+		// テストのための準備としてデータベースに接続する。
+
+		try {
+			con = ConnectionManager.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// ここからテストを行う。
+		try {
+			CustomerCodeDAO custDAO = new CustomerCodeDAO(con);
+			boolean custList = custDAO.updateCustomerCode(custDAO.findCustomerCode()+1);
+
+			System.out.println(custList);
+
+		} catch (SQLException e) {
+			System.out.println("SQLExceptionがスローされました。");
+			e.printStackTrace();
+		} finally {
+			try {// データベースへの接続を切断する
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
